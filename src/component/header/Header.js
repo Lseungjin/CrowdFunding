c603d7ccd3e60dbbe2deafe1b522ea9f7ec5167f
Lseungjin <promotion0602@naver.com>
2023-12-14 20:18:34 +0900

    1차 수정

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -4,9 +4,78 @@ import MetamaskPNG from "../../assets/metamask.png"
 
 const Header = (props) => {
     const [showModal, setShowModal] = useState(false);
-    const [name, setName] = useState('');
-    const [address, setAddress] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [metamaskAddress, setMetamaskAddress] = useState('');
 
+    const handleNicknameChange = (event) => {
+        setNickname(event.target.value);
+    };
+
+     // 닉네임 유효성 확인
+    async function checkName() {
+        // 액세스 토큰 가져오기
+        const localStorage = window.localStorage;
+        const token = localStorage.getItem("accessToken");
+    
+        // 닉네임 중복 확인 API
+        const nameCheckAPI = `https://funsns.shop:8000/user-service/metamask/${nickname}`;
+    
+        // fetch API
+        let res = await fetch(nameCheckAPI, {
+            method: "GET",
+            headers: {
+                Credentials: "include",
+                ContentType: "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                alert("유효한 닉네임입니다.");
+                setValidName(true);
+            } else {
+                alert("존재하지 않는 닉네임입니다.");
+                setValidName(false);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            throw new Error("서버 요청 실패!");
+        });
+    }
+
+     // 메타마스크 주소 등록
+    async function registerMetamaskAddress() {
+        const localStorage= window.localStorage;
+        const token = localStorage.getItem("accessToken");
+
+        const registerAPI = `https://funsns.shop:8000/user-service/metamask/address`;
+
+        let res = await fetch(registerAPI, {
+            method: "POST",
+            headers: {
+                Credentials: "include",
+                ContentType: "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                "nickname": nickname,
+                "metamaskAddress": address
+            })
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                alert("메타마스크 주소가 등록되었습니다.");
+            } else {
+                alert("메타마스크 주소 등록에 실패하였습니다.");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            throw new Error("서버 요청 실패!");
+        });
+    }
     return (
         <div className="header-container">
             <div className="search-bar">
@@ -48,17 +117,27 @@ const Header = (props) => {
                         <div className="ip-fields">
                             <input
                                 type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder="이름"></input>
+                                value={nickname}
+                                onChange={handleNicknameChange}
+                                placeholder="닉네임"></input>
                         </div>
+                        <button
+                            onClick={checkName}
+                            className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+                            닉네임 유효성 확인 버튼
+                        </button>
                         <div className="ip-fields">
                             <input
                                 type="text"
-                                value={address}
-                                onChange={(e) => setAddress(e.target.value)}
+                                value={metamaskAddress}
+                                onChange={(e) => setMetaMaskAddress(e.target.value)}
                                 placeholder="메타마스크 주소"></input>
                         </div>
+                        <button
+                            onClick={registerMetamaskAddress}
+                            className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+                            메타마스크 주소 등록
+                        </button>
                     </div>
                 </div>
             )}
